5377b2c601a33e55d0ce2fe6115561fb54df6dbb
316136035 <316136035@qq.com>
2023-12-07 22:23:17 +0800

    A001

--- A03_nodejs/A03_fs文件模块操作_目录操作/A02_fs文件模块_写入.js
+++ A03_nodejs/A03_fs文件模块操作_目录操作/A02_fs文件模块_写入.js
@@ -9,13 +9,13 @@ const  fs=require('fs');
 //     console.log("写入成功")
 // });
 // //异步写入文件，这种写法会追加整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容"，配置对象,回填函数）   
-fs.writeFile('./A02_fs文件模块.text',`配置对象,{flag:'a'}` ,{flag:'a'},err=>{
-    // err 错误对象  成功的话 err为null
-    if(err){
-        console.log("配置对象写入失败")
-    }
-    console.log("配置对象写入成功")
-});
+// fs.writeFile('./A02_fs文件模块.text',`配置对象,{flag:'a'}` ,{flag:'a'},err=>{
+//     // err 错误对象  成功的话 err为null
+//     if(err){
+//         console.log("配置对象写入失败")
+//     }
+//     console.log("配置对象写入成功")
+// });
 // //异步写入文件，这种写法追加整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容"，回填函数）   
 // fs.appendFile('/A02_fs文件模块.text',`瀛洲学士风流远appendFile` ,err=>{
 //     // err 错误对象  成功的话 err为null
@@ -30,8 +30,8 @@ fs.writeFile('./A02_fs文件模块.text',`配置对象,{flag:'a'}` ,{flag:'a'},e
 // //同步写入文件，这种写法追加整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容")
 // fs.appendFileSync('/A02_fs文件模块.text',`appendFileSync` )
 
-//创建文件输出流（写入流，异步写入文件）
-const WS =fs.createWriteStream('./A02_fs文件模块.txt')
+//创建文件输出流（写入流，异步写入文件） ({ flags: 'a' } 追加写入)
+const WS =fs.createWriteStream('./A02_fs文件模块.txt' ,{ flags: 'a' })
 //流式写入
 WS.write("A\r\n")
 WS.write("B\r\n")
