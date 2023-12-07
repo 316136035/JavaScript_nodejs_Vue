c729b3b1e0ce639822ad9ce9964f8f0766ac6fcf
316136035 <316136035@qq.com>
2023-12-07 22:33:59 +0800

    A001

--- A03_nodejs/A03_fs文件模块操作_目录操作/A05_fs文件模块_删除文件.js
+++ A03_nodejs/A03_fs文件模块操作_目录操作/A05_fs文件模块_删除文件.js
@@ -2,12 +2,12 @@
 const fs = require('fs');
 
 //异步删除文件 （要删除的文件路径，回调函数） //同步删除文件使用unlinkSync
-fs.unlink('./A04_借口 copy.ogg', error => {
+fs.unlink('./A02_fs文件模块.txt', error => {
     if (error) { console.log("删除失败") }
     console.log("删除成功")
 })
-//异步删除文件 （要删除的文件路径，回调函数） //同步删除文件使用rmSync
-fs.rm('./A04_借口 copy.ogg', error => {
+//异步删除空目录 （要删除的空目录的路径，回调函数） //同步删除文件使用rmSync
+fs.rm('./A02_fs文件模块.txt', error => {
         if (error) { console.log("删除失败") }
         console.log("删除成功")
     }
