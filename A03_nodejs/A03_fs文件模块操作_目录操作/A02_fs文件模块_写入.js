07ace2f3342548213d830875a91c0a6b0eed743b
316136035 <316136035@qq.com>
2023-12-07 22:36:42 +0800

    A001

--- A03_nodejs/A03_fs文件模块操作_目录操作/A02_fs文件模块_写入.js
+++ A03_nodejs/A03_fs文件模块操作_目录操作/A02_fs文件模块_写入.js
@@ -1,7 +1,7 @@
 //导入fs文件模块 （nodejs只带不用安装）
 const  fs=require('fs');
 // //异步写入文件，这种写法会替换整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容"，回填函数）   
-// fs.writeFile('/A02_fs文件模块.text',`瀛洲学士风流远` ,err=>{
+// fs.writeFile('./A02_fs文件模块.txt',`瀛洲学士风流远` ,err=>{
 //     // err 错误对象  成功的话 err为null
 //     if(err){
 //         console.log("写入失败")
@@ -9,7 +9,7 @@ const  fs=require('fs');
 //     console.log("写入成功")
 // });
 // //异步写入文件，这种写法会追加整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容"，配置对象,回填函数）   
-// fs.writeFile('./A02_fs文件模块.text',`配置对象,{flag:'a'}` ,{flag:'a'},err=>{
+// fs.writeFile('./A02_fs文件模块.txt',`配置对象,{flag:'a'}` ,{flag:'a'},err=>{
 //     // err 错误对象  成功的话 err为null
 //     if(err){
 //         console.log("配置对象写入失败")
@@ -17,7 +17,7 @@ const  fs=require('fs');
 //     console.log("配置对象写入成功")
 // });
 // //异步写入文件，这种写法追加整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容"，回填函数）   
-// fs.appendFile('/A02_fs文件模块.text',`瀛洲学士风流远appendFile` ,err=>{
+// fs.appendFile('./A02_fs文件模块.txt',`瀛洲学士风流远appendFile` ,err=>{
 //     // err 错误对象  成功的话 err为null
 //     if(err){
 //         console.log("追加写入失败")
@@ -26,9 +26,9 @@ const  fs=require('fs');
 // })
 
 // //同步写入文件，这种写会法替换整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容")
-// fs.writeFileSync('/A02_fs文件模块.text',`瀛洲学士风流远Sync` )
+// fs.writeFileSync('./A02_fs文件模块.txt',`瀛洲学士风流远Sync` )
 // //同步写入文件，这种写法追加整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容")
-// fs.appendFileSync('/A02_fs文件模块.text',`appendFileSync` )
+// fs.appendFileSync('./A02_fs文件模块.txt',`appendFileSync` )
 
 //创建文件输出流（写入流，异步写入文件） ({ flags: 'a' } 追加写入)
 const WS =fs.createWriteStream('./A02_fs文件模块.txt' ,{ flags: 'a' })
