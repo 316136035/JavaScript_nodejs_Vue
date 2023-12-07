f1bdf019d1a8ff69b0ec0b5a5a76475781e49022
316136035 <316136035@qq.com>
2023-12-07 22:20:11 +0800

    A001

--- A03_nodejs/A03_fs文件模块操作_目录操作/A01_fs文件模块_读取.js
+++ A03_nodejs/A03_fs文件模块操作_目录操作/A01_fs文件模块_读取.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 //异步读取 （路径，(失败回调+成功回调)=>{}）
-fs.readFile('./A04_fs文件模块.txt', (err, data) => {
+fs.readFile('.A01_fs文件模块_读取.js', (err, data) => {
      // err 错误对象  成功的话 err为null
     if (err) { throw err }
     //读取为Budder数组  转成字符串
@@ -12,13 +12,12 @@ fs.readFile('./A04_fs文件模块.txt', (err, data) => {
 
 
 //同步读取 读取为Budder数组 
-let data= fs.readFileSync('./A04_fs文件模块.txt')
+let data= fs.readFileSync('A01_fs文件模块_读取.js')
 // Budder数组 转成字符串
 console.log("同步读取: ",data.toString())
 
-//创建文件输入流（读取流）
-const rs= fs.createReadStream('./A04_借口.ogg');
-
+//创建文件输入流（读取流，异步读取）
+const rs= fs.createReadStream('A01_fs文件模块_读取.js');
 //绑定读取成功回调  （每读取一组就调用一次这个回调）
 rs.on('data',data=>{
     //读取的是buffer数组
@@ -27,7 +26,7 @@ rs.on('data',data=>{
 })
 //绑定读取失败回调
 rs.on('error', err=>{
-    console.log("读取失败..")
+    console.log("读取失败..",err)
 })
 //绑定读取读取完毕
 rs.on('end',()=>{
--- A03_nodejs/A03_fs文件模块操作_目录操作/A02_fs文件模块_写入.js
+++ A03_nodejs/A03_fs文件模块操作_目录操作/A02_fs文件模块_写入.js
@@ -1,7 +1,7 @@
 //导入fs文件模块 （nodejs只带不用安装）
 const  fs=require('fs');
 // //异步写入文件，这种写法会替换整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容"，回填函数）   
-// fs.writeFile('./A04_fs文件模块.txt',`瀛洲学士风流远` ,err=>{
+// fs.writeFile('/A02_fs文件模块.text',`瀛洲学士风流远` ,err=>{
 //     // err 错误对象  成功的话 err为null
 //     if(err){
 //         console.log("写入失败")
@@ -9,15 +9,15 @@ const  fs=require('fs');
 //     console.log("写入成功")
 // });
 // //异步写入文件，这种写法会追加整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容"，配置对象,回填函数）   
-// fs.writeFile('./A04_fs文件模块.txt',`配置对象,{flag:'a'}` ,{flag:'a'},err=>{
-//     // err 错误对象  成功的话 err为null
-//     if(err){
-//         console.log("配置对象写入失败")
-//     }
-//     console.log("配置对象写入成功")
-// });
+fs.writeFile('./A02_fs文件模块.text',`配置对象,{flag:'a'}` ,{flag:'a'},err=>{
+    // err 错误对象  成功的话 err为null
+    if(err){
+        console.log("配置对象写入失败")
+    }
+    console.log("配置对象写入成功")
+});
 // //异步写入文件，这种写法追加整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容"，回填函数）   
-// fs.appendFile('./A04_fs文件模块.txt',`瀛洲学士风流远appendFile` ,err=>{
+// fs.appendFile('/A02_fs文件模块.text',`瀛洲学士风流远appendFile` ,err=>{
 //     // err 错误对象  成功的话 err为null
 //     if(err){
 //         console.log("追加写入失败")
@@ -26,14 +26,13 @@ const  fs=require('fs');
 // })
 
 // //同步写入文件，这种写会法替换整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容")
-// fs.writeFileSync('./A04_fs文件模块.txt',`瀛洲学士风流远Sync` )
+// fs.writeFileSync('/A02_fs文件模块.text',`瀛洲学士风流远Sync` )
 // //同步写入文件，这种写法追加整个文件的内容（文件路径(文件不存在就会创建新的文件，文件存在覆盖)，"文件内容")
-// fs.appendFileSync('./A04_fs文件模块.txt',`appendFileSync` )
+// fs.appendFileSync('/A02_fs文件模块.text',`appendFileSync` )
 
-//创建文件输出流（写入流）
+//创建文件输出流（写入流，异步写入文件）
 const WS =fs.createWriteStream('./A02_fs文件模块.txt')
 //流式写入
-
 WS.write("A\r\n")
 WS.write("B\r\n")
 WS.write("C\r\n")
