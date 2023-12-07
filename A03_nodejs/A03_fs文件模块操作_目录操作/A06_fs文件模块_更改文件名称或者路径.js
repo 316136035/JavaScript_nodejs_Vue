fc768a272b6f7cb5b79484b07e8c3e32c9dd2a2d
316136035 <316136035@qq.com>
2023-12-07 22:40:13 +0800

    A001

--- A03_nodejs/A03_fs文件模块操作_目录操作/A06_fs文件模块_更改文件名称或者路径.js
+++ A03_nodejs/A03_fs文件模块操作_目录操作/A06_fs文件模块_更改文件名称或者路径.js
@@ -1,7 +1,7 @@
 //导入fs文件模块 （nodejs只带不用安装）
 const fs = require('fs');
 //更改文件名称或者路径（'原文件路径'，'修改后路径文件名'，回调方法）
-fs.rename('./A04_借口1.ogg','../借口.ogg', error=>{
+fs.rename('./A02_fs文件模块.txt','./A02_fs文件模块.txt', error=>{
     if(error){
         console.log("修改文件名失败")
         return}
