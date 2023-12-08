c17b0ee5be2c4e431030f28dfaa1a8dda7558b75
316136035 <316136035@qq.com>
2023-12-08 12:20:30 +0800

    A001

--- A03_nodejs/A03_fs文件模块操作_目录操作/A09_fs文件模块_练习.js
+++ A03_nodejs/A03_fs文件模块操作_目录操作/A09_fs文件模块_练习.js
@@ -1,14 +1,14 @@
 //导入fs文件模块 （nodejs只带不用安装）
 const fs = require('fs');
 //读取目录中的文件夹/文件
-fs.readdir('../A02_fs文件模块操作_目录操作', (err, data) => {
-    if (err) { return }
+fs.readdir('../A03_fs文件模块操作_目录操作', (err, data) => {
+    if (err) { console.log(err); }
     //变量文件夹/文件数组
     data.forEach((item) => {
         //通过-拆分文件名
         let data1 = item.split('_')
         //结构赋值
-        let [num, fs, name] = data1
+        let [num, fs, name] = data1; //num 序号  fs 文件夹/文件  name 文件名
         console.log(num, '    ', fs, '    ', name)
     })
 
