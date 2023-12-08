02e8d40c4063a8e8df4131058f3b09154e11645a
316136035 <316136035@qq.com>
2023-12-08 21:16:53 +0800

    A001

--- A04_nodejs/A05_http模块-超文本传输协议/A05_设置http响应报文.js
+++ A04_nodejs/A05_http模块-超文本传输协议/A05_设置http响应报文.js
@@ -9,7 +9,7 @@ const server = http.createServer((request, response) => {
     //设置响应头 浏览器可以接收类型
     response.setHeader('content-Type', 'text/html;charset=utf-8;')
     //一般在这里写入响应体就不会在end中写入响应体
-    response.write('服务器启动成功')
+    response.write('服务器启动成功.........')
     response.write('        一般在这里写入响应体就不会在end中写入响应体')
     //响应
     response.end()
@@ -17,7 +17,7 @@ const server = http.createServer((request, response) => {
 })
 //服务器--->监听服务  HTTP协议默认端口是80。HTTPS协议的默认端口是443，HTTP服务开发常用端口有3000，8080， 8090,9000等
 //(端口, 域名，函数）
-server.listen(80,'taoshenjia.vip', () => {
+server.listen(80,'127.0.0.1', () => {
     console.log("服务器启动成功.. http://127.0.0.1:80")
 
 })
