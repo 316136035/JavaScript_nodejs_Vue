c9446c0504b5d7798f4b87b690e24906d3f64f4f
316136035 <316136035@qq.com>
2023-12-09 23:39:59 +0800

    A001

--- A04_nodejs/A05_http模块-超文本传输协议/A03_获取url和路径和传参.js
+++ A04_nodejs/A05_http模块-超文本传输协议/A03_获取url和路径和传参.js
@@ -6,16 +6,16 @@ const url = require('url')
 const server = http.createServer((request, response) => {  ////设置响应头  设置浏览器可以接收类型，  设置编码（防止乱码）
     response.setHeader('content-Type', 'text/html;charset=utf-8;')//设置响应头  设置浏览器可以接收类型，  设置编码（防止乱码）
     let urlparse = url.parse(request.url) // url.parse()方法把url解析成一个对象
-    console.log('url对象:',urlparse)
-    //用url模块解释url  第2个参数是true key_value就为对象
+    console.log('url.parse()函数获取url对象:', urlparse)
+    //用url模块解释url  方法1  第2个参数是true query参数会变成对象
     let urlparsetrue = url.parse(request.url, true)
-    //获取url中的路径
-    console.log(urlparse.pathname)
+    console.log('url.parse()函数获取url中的路径:', urlparse.pathname)//获取url中的路径
+    console.log('url.parse()函数获取url中的参数:',  urlparse.query)//获取url中的参数（对象）
 
     //模块解释url  方法2  ('/路径？key=value'，后面这个参数就是补全URL用的)
     const newurl = new URL(request.url, 'http://127.0.0.1')
     //打印完整的url
-    // console.log(newurl)
+    console.log(newurl)
     //获取路径
     console.log(newurl.pathname)
     //通过key获取value
@@ -26,6 +26,6 @@ const server = http.createServer((request, response) => {  ////设置响应头  
 })
 //服务器--->监听服务  HTTP协议默认端口是80。HTTPS协议的默认端口是443，HTTP服务开发常用端口有3000，8080， 8090,9000等
 //(端口, 域名，函数）
-server.listen(3000,'127.0.0.1', () => {
-    console.log("服务器启动成功.. http://127.0.0.1:3000/A03?key=value")
+server.listen(3000, '127.0.0.1', () => {
+    console.log("服务器启动成功.. http://127.0.0.1:3000/A03?key1=value1&key2=value2")
 })
