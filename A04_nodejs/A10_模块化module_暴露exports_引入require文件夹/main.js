233e5b269b54168662867ab5224edad0e9051336
316136035 <316136035@qq.com>
2023-12-12 13:15:08 +0800

    A001

--- A04_nodejs/A10_模块化module_暴露exports_引入require文件夹/main.js
+++ A04_nodejs/A10_模块化module_暴露exports_引入require文件夹/main.js
@@ -1,4 +1,4 @@
-//导入文件模块  （如果文件夹中main属性不存在，或者package.,json不存在则会尝试导入文件夹下的 index.js和index.json ,）
+//导入文件模块  （如果文件夹中main属性不存在，或者package.,json不存在则会尝试导入文件夹下的 index.js和index.json ,不然就报错）
 //main中引入模块
 const modu=require('./module')
 
