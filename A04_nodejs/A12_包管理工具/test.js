4ffa2f87cc64e2a856dc1e6c42ae8a56559b848d
316136035 <316136035@qq.com>
2023-12-12 17:11:17 +0800

    A001

--- A04_nodejs/A12_包管理工具/test.js
+++
@@ -1,2 +0,0 @@
-const tao=require('tao_shen_jia')
-console.log(tao.functionA(1,2) )
---
+++ A04_nodejs/A12_包管理工具/安装包/index.js
@@ -0,0 +1,6 @@
+//引入模块
+//1.当前文件夹下 node_modules 中寻找同名的文件夹
+//2，在上级目录中下的 node_modules 中寻找同名的文件夹，直至找到磁盘根目录
+const math=require('math');
+
+console.log( math.samesign(1,2));
