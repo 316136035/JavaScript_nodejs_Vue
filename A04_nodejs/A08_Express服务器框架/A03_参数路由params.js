9cc1ecea23bef4be08571eedeac6407c0d757812
316136035 <316136035@qq.com>
2023-12-14 12:21:17 +0800

    A001

--- A04_nodejs/A08_Express服务器框架/A03_参数路由params.js
+++ A04_nodejs/A08_Express服务器框架/A03_参数路由params.js
@@ -1,14 +1,33 @@
-// 路由参数params
-const express = require('./express');
-const server = express();
-server.get(':id', (req, res) => {
-  let { id } = req.params;
-  console.log(req.params)
-  res.send('user')
-})
+// 引入express服务器框架
+const express = require('express');
+// 引入json文件
+const jsonObject = require('./publi公共目录/A03_参数路由params.json');
 
-server.listen(3000, '12070.0.1', () => {
+// 创建服务器
+const server = express();
+// 路由规则（:id占位符 可以有多个占位符）
+server.get('/user/:id.html', (req, res) => {
+
+  let paramsobject = req.params;//获取参数对象{ id: '1' }
+
+  let { id } = paramsobject;//对象_解构 获取id参数
+  let { persons } = jsonObject;//对象_解构 获取persons参数
+
+  //find方法还可以用于遍历对象数组  返回布尔值 布尔值是符合条件的第一个的对象
+  let person = persons.find((item) => {
+    if (item.id == Number(id)) {
+      return true;
+    }
+  })
+  res.send(person);//渲染页面
+
+})
+// 监听端口
+server.listen(3000, '127.0.0.1', () => {
   console.log('服务器启动成功')
+  console.log(`路由参数获取；'http://127.0.0.1:3000/user/1.html'`)
+  console.log(`路由参数获取；'http://127.0.0.1:3000/user/2.html'`)
+  console.log(`路由参数获取；'http://127.0.0.1:3000/user/3.html'`)
 })
 
 
