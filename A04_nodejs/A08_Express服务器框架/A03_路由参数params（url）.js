47fbb8754b60b4996fbf0b6b084dfba949ddf575
316136035 <316136035@qq.com>
2023-12-15 10:18:42 +0800

    A001

--- A04_nodejs/A08_Express服务器框架/A03_路由参数params（url）.js
+++ A04_nodejs/A08_Express服务器框架/A03_路由参数params（url）.js
@@ -1,18 +1,15 @@
 // 引入express服务器框架
 const express = require('express');
 // 引入json文件
-const jsonObject = require('./publi公共目录/A03_参数路由params.json');
+const jsonObject = require(__dirname+'/publi/A03_参数路由params.json');
 
 // 创建服务器
 const server = express();
 // 路由规则（:id占位符 可以有多个占位符）
 server.get('/user/:id.html', (req, res) => {
-
   let paramsobject = req.params;//获取参数对象{ id: '1' }
-
   let { id } = paramsobject;//对象_解构 获取id参数
   let { persons } = jsonObject;//对象_解构 获取persons参数
-
   //find方法还可以用于遍历对象数组  返回布尔值 布尔值是符合条件的第一个的对象
   let person = persons.find((item) => {
     if (item.id == Number(id)) {
