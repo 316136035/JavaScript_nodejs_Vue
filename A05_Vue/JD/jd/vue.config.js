40f2d16df1f6384723bc422a0a6b37d7db5e3e3c
316136035 <316136035@qq.com>
2023-12-15 22:54:50 +0800

    A001

--- A05_Vue/JD/jd/vue.config.js
+++ A05_Vue/JD/jd/vue.config.js
@@ -65,6 +65,7 @@ module.exports = defineConfig({
         pathRewrite: { "^/get_token_eid": "" },
         secure: false,           //如果是https接口，需要配置这个参数 
         changeOrigin: true,  // 如果接口跨域，需要进行这个参数配置
+       
         headers: {
           Host: 'gia.jd.com',
           Connection: 'keep-alive',
