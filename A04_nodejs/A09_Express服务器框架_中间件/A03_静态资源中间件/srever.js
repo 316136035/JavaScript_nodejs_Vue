6937f19ee754b02ecf0f62395c4e72ea9e16b696
316136035 <316136035@qq.com>
2023-12-15 11:46:17 +0800

    A001

--- A04_nodejs/A09_Express服务器框架_中间件/A03_静态资源中间件/srever.js
+++ A04_nodejs/A09_Express服务器框架_中间件/A03_静态资源中间件/srever.js
@@ -7,6 +7,7 @@ server.use(express.static(__dirname+'/public'));
 server.get('/',(req,res)=>{
     res.send("响应成功..")
 });
+//监听端口
 server.listen(3000,`127.0.0.1`,()=>{
   console.log('服务器启动成功..http://127.0.0.1:3000/index.html',) 
 })
