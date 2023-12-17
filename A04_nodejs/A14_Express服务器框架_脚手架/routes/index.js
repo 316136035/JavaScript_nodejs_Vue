9a977974c77456c5d3c7b2729a7cdefbb46dbe93
316136035 <316136035@qq.com>
2023-12-17 19:53:47 +0800

    A001

--- A04_nodejs/A14_Express服务器框架_脚手架/routes/index.js
+++ A04_nodejs/A14_Express服务器框架_脚手架/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router(); //获取路由实例对象
 
 /* 主页路由*/
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });//
+  const str ='淘神价';
+   //res.render('模版名称：index.ejs',{变量名:数组arr});
+   //模版名称在views目录中
+  res.render('index', { title: str});
 });
 // 暴露路由模块
 module.exports = router;
