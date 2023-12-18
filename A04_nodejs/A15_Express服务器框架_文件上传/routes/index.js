e6fd0e1ed5b31bc0182966d71da168c67538e9f0
316136035 <316136035@qq.com>
2023-12-18 22:17:28 +0800

    A001

--- A04_nodejs/A15_Express服务器框架_文件上传/routes/index.js
+++ A04_nodejs/A15_Express服务器框架_文件上传/routes/index.js
@@ -8,18 +8,22 @@ router.get('/', function(req, res, next) {
 /* post 主页  文件上传页面*/
 router.post('/upload/file', (req, res, next) => {
   // 解析请求体
-  const form = formidable({});
+  const form = formidable({
+    multiples:true,//支持上传多个文件
+    uploadDir:__dirname+'/../public/images',//保存路径
+    keepExtensions:true,//保留后缀
+  });
   // 监听请求体事件
   form.parse(req, (err, fields, files) => {
     if (err) {
       next(err);
       return;
     }
-    console.log(fields, files);// 打印请求体 信息（fields 输入框信息，files 文件信息）
-  
+    console.log(fields, files);// 打印请求体 信息（fields 存放/text/password/textarea/select/ radio/checkbox/select，
+    console.log(files);//files文件信息 
   });
   // 响应
-  res.send('ok');
+   res.send('ok');
   });
 // 暴露路由
 module.exports = router;
