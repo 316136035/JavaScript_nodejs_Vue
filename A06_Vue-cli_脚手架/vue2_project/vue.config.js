f69e01eb1cd1beba1ef4f256882e4535788a2cb8
316136035 <316136035@qq.com>
2024-02-20 13:11:22 +0800

    A001

--- A06_Vue-cli_脚手架/vue2_project/vue.config.js
+++ A06_Vue-cli_脚手架/vue2_project/vue.config.js
@@ -1,3 +1,4 @@
+//暴露配置
 module.exports = {
   pages: {
     index: {
@@ -9,19 +10,17 @@ module.exports = {
       filename: 'index.html',
       // 当使用 title 选项时，
       // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      // title: 'Index Page',
+      title: 'Index Page',
       // 在这个页面中包含的块，默认情况下会包含
       // 提取出来的通用 chunk 和 vendor chunk。
-      // chunks: ['chunk-vendors', 'chunk-common', 'index']
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
     },
     // 当使用只有入口的字符串格式时，
     // 模板会被推导为 `public/subpage.html`
     // 并且如果找不到的话，就回退到 `public/index.html`。
     // 输出文件名会被推导为 `subpage.html`。
-    // subpage: 'src/subpage/main.js'
- }
+    //subpage: 'src/subpage/main.js'
+  },
+  lintOnSave:false,//关闭语法检查
+
 }
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
- lintOnSave:false //关闭语法检查
-})
