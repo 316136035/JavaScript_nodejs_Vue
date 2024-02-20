898545537805c67fa2677ead7b588cab30649d1e
316136035 <316136035@qq.com>
2024-02-20 20:53:45 +0800

    A001

---
+++ A06_Vue-cli_脚手架/vue2_project/A04_src_props配置对象接受父组件传给子组件的数据/App.vue
@@ -0,0 +1,40 @@
+<!-- 模版 -->
+<template>
+  <div id="app">
+    <!-- 创建组件 并传入参数    :符号为表达-->
+    <Student :name="'淘神价vip'" :age="18" :gender="'男'" ></Student>
+ </div>
+</template>
+
+<script>
+// 引入组件
+import Student from './components/Student.vue'
+///暴露主组件的变量和方法 等等  -->
+export default {
+  name: 'App',//自定义组件名称
+  // 注册组件
+  components: {
+    Student: Student
+  },
+  data() {
+    return {
+      msg: '欢迎来到淘神价'
+    }
+  },
+  // 定义方法
+  methods: { }
+  }
+
+</script>
+
+<!-- css样式 -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ A06_Vue-cli_脚手架/vue2_project/A04_src_props配置对象接受父组件传给子组件的数据/components/Student.vue
@@ -0,0 +1,40 @@
+<!-- 学校信息模版 -->
+<template>
+  <div class="Student">
+    <div class="Student-name">{{  myname  }}</div>
+    <div class="Student-age">{{ age+1 }}</div>
+    <div class="Student-address">{{ gender }}</div>
+  </div>
+</template>
+
+<script>
+//暴露子组件的变量和方法 等等  -->
+export default {
+  data() {
+    return { myname :this.name //传入的参数赋值给 myname
+    }
+  },
+  /**声明接受数据三种写法 传入的参数不建议修改  备注: props是只读的，Vue底层会监测你对props的修改，如果进行了修改，就会发出警告，若业务需求确实需要修改，那么请复制props的内容到data中一份，然后去修改data中的数据。*/
+  /*1.简单写法 */
+  props: ["name", "age", "gender"],
+  /*2.声明接受的同时对数据进行类型限制*/
+  props: {
+    name: String, //限制为字符串
+    age: Number, //限制为数字
+    gender: String,//限制为字符串
+  },
+  /*3.声明接受的同时对数据进行类型限制和必要性的限制*/
+  props: {
+    name: { String, required: true }, //声明接受字符串，必填
+    age: { Number, default: 100 }, //声明接受数字，非必填有默认值
+    gender: { String, required: false }, //声明接受字符串，非必填
+  },
+};
+</script>
+<!-- css -->
+<style>
+.school-name {
+  font-size: 40px;
+  color: red;
+}
+</style>
