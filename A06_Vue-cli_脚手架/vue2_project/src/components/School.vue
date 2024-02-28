9b44dace05472ac90ae1daa847b20128748c0814
316136035 <316136035@qq.com>
2024-02-28 14:41:39 +0800

    A001

---
+++ A06_Vue-cli_脚手架/vue2_project/src/components/School.vue
@@ -0,0 +1,39 @@
+<!-- 模版 -->
+<template>
+  <div class="School">
+    <h1>我是学校组件,学生列表</h1>
+    <div>{{ student }}</div>
+  </div>
+</template>
+
+<script>
+//暴露组件的变量和方法 等等  -->
+export default {
+  name: "School",
+  data() {
+    return {
+      student: null,
+    };
+  },
+  methods: {
+    //定义函数
+    getStudent(student) {
+      //把子组件传过来的对象赋值给父组件的student对象
+      this.student = student;
+      console.log(JSON.stringify(student));
+    },
+  },
+  //Vue完成模板的解析并把初始的真实DOM元素放入页面后(挂载完毕)， 该钩子函数会被调用。
+  mounted() {
+    //在VUE的原型上的$bus 添加一个事件总线
+    this.$bus.$on("getStudent", this.getStudent);
+  },
+};
+</script>
+
+<!-- scoped代表只能在当前组件使用的css样式 -->
+<style scoped>
+#School {
+  background-color: red;
+}
+</style>
