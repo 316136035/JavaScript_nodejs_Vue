5e9b18d0bfe4a626ac915cfc72cfba00598efa74
316136035 <316136035@qq.com>
2024-02-29 21:23:58 +0800

    A001

--- A06_Vue-cli_脚手架/vue2_project/A13_src _动画和过渡和三方动画和过渡的使用/App.vue
+++ A06_Vue-cli_脚手架/vue2_project/A13_src _动画和过渡和三方动画和过渡的使用/App.vue
@@ -7,6 +7,9 @@
     <Transition></Transition>
     <hr>
     <TransitionList></TransitionList>
+    <hr>
+    <hr>
+    <Three_party_package></Three_party_package>
   </div>
 </template>
 
@@ -15,6 +18,10 @@
 import Animation from './components/Animation.vue'
 import Transition from './components/Transition.vue'
 import TransitionList from './components/TransitionList.vue'
+import Three_party_package from './components/Three_party_package.vue'
+// 引入第三方包
+import 'animate.css'
+//安装包
 // 暴露组件
 export default {
   name: 'App',//自定义组件名称
@@ -22,7 +29,9 @@ export default {
   components: {
     Animation:Animation,
     Transition:Transition,
-    TransitionList:TransitionList
+    TransitionList:TransitionList,
+    Three_party_package:Three_party_package
+    
 
   }
 }
---
+++ A06_Vue-cli_脚手架/vue2_project/A13_src _动画和过渡和三方动画和过渡的使用/components/Three_party_package.vue
@@ -0,0 +1,47 @@
+<!-- 模版 -->
+<template>
+  <div class="hello">
+     <div id="transition">三方包实现动画和过渡</div>
+    <button v-on:click="isshow = !isshow">显示/隐藏</button>
+    <br />
+   <!--transition-group 过渡效果多个元素 
+      name="animate__animated animate__bounce"(必填)  
+      enter-active-class="进入的动画" 
+      leave-active-class="离开的动画""-->
+    <transition
+     name="animate__animated animate__bounce"  
+     enter-active-class="animate__backInDown"
+     leave-active-class="animate__backOutDown"  >
+    <div class="container" v-show="isshow">三方包实现动画和过渡</div>
+    </transition>
+ </div>
+</template>
+<script>
+// 引入第三方包
+import 'animate.css'
+
+//暴露组件的变量和方法 等等  -->
+export default {
+  name: "Three_party_package",
+  data() {
+    return {
+      isshow: true,
+    };
+  },
+};
+</script>
+<!-- scoped代表只能在当前组件使用的css样式 -->
+<style scoped>
+/* 默认样式 */
+#transition{
+  font-size: 40px;
+}
+/* 默认样式 */
+.container {
+  height: 100px;
+  background-color: darkorange;
+ margin: 10px;
+  transition: all 0.9s linear;
+}
+
+</style>
