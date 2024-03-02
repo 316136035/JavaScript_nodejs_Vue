7b3a90a758aea4870ae60268733e9f1956372f90
316136035 <316136035@qq.com>
2024-03-02 17:04:16 +0800

    A001

--- A06_Vue-cli_脚手架/Server/server.js
+++ A06_Vue-cli_脚手架/Server/server.js
@@ -2,32 +2,35 @@
 var express = require('express');//引入express框架
 var path = require('path');//引入path路劲操作
 var cookieParser = require('cookie-parser');//引入cookies 模块
+const { log } = require('console');
 //创建应用对象
 const server = express();
 //创建路由 (匹配 ip+端口)
 server.get('/HomePage', (request, response) => {
 
 
-    const requestText={
-        path:request.path,
-        query:request.query,
-        headers:request.headers,
-        cookies:request.cookies,
-        method:request.method,
-        ip:request.ip,
-        url:request.url,
-        body:request.body,
-        params:request.params,
-  }
+    const requestText = {
+        url: request.url,//请求url
+        method: request.method,//请求方法
+        path: request.path,//请求路径
 
+        params: request.params,//请求参数
+        ip: request.ip,//请求ip
+        headers: request.headers,//请求头
+        cookies: request.cookies,//请求cookies
+        body: request.body,//请求体
+        ha:"你好"
+
+    }
+    console.log(JSON.stringify(requestText));
     response.send(JSON.stringify(requestText))
 
 })
 
 
 //服务器--->监听服务  HTTP协议默认端口是80。HTTPS协议的默认端口是443，HTTP服务开发常用端口有3000，8080， 8090,9000等
-server.listen(4000, "127.0.0.1",() => {
+server.listen(4000, "127.0.0.1", () => {
     console.log('服务器已经启动...http://127.0.0.1:4000/HomePage/')
 
-   
+
 })
--- A06_Vue-cli_脚手架/vue2_project/src/components/SingleSpanningRrequest.vue
+++ A06_Vue-cli_脚手架/vue2_project/src/components/SingleSpanningRrequest.vue
@@ -3,7 +3,19 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div>
-      {{ requestText }}
+     
+      <div>请求url{{ requestText.url }}</div>
+      <div>请求方法{{ requestText.method }}</div>
+      <div>求路径{{ requestText.path }}</div>
+      <div>请求参数{{ requestText.params }}</div>
+      <div>请求ip{{ requestText.ip }}</div>
+      <hr>
+      <div>请求头参数</div>
+      <div v-for="(item, index) in requestText.headers" :key="index">
+        <div>{{ item }}</div>
+      </div>
+      <div>{{ requestText.cookies }}</div>
+      <div>{{ requestText.body }}</div>
     </div>
     <button v-on:click="span">发送跨越axios请求单个配置</button>
   </div>
@@ -18,7 +30,7 @@ export default {
   data() {
     return {
       msg: "跨越请求处理方案一",
-      requestText:""
+      requestText: Object,
     };
   },
   methods: {
@@ -33,7 +45,8 @@ export default {
         })
         //成功回调函数
         .then((data) => {
-           this.requestText = data;
+          console.log(data.request.responseText);
+          this.requestText = JSON.parse(data.request.responseText);
         });
     },
   },
