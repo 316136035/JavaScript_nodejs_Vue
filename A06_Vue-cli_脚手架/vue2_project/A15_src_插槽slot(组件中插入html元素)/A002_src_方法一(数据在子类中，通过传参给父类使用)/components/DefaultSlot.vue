b5431f9020e359ec7152d30c446cbfb9356afaa4
316136035 <316136035@qq.com>
2024-03-05 17:36:40 +0800

    A001

--- A06_Vue-cli_脚手架/vue2_project/A15_src_插槽slot(组件中插入html元素)/A002_src_方法一(数据在子类中，通过传参给父类使用)/components/DefaultSlot.vue
+++ A06_Vue-cli_脚手架/vue2_project/A15_src_插槽slot(组件中插入html元素)/A002_src_方法一(数据在子类中，通过传参给父类使用)/components/DefaultSlot.vue
@@ -1,7 +1,7 @@
 <!-- 模版 -->
 <template>
   <div>
-    <!-- 定义插槽slot 名称为DefaultSlot  :foods=foods把数据传给父组件 -->
+    <!-- 定义插槽slot 名称为DefaultSlot  :foods=foods把数据传给父组件（插槽特有的传参方式） -->
     <slot name="DefaultSlot" :foods=foods>我是一个插槽DefaultSlot，当使用者没有传递具体结构时，我会出现.</slot>
   </div>
 </template>
