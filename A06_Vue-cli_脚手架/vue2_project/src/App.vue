35683aedc2a89c8c4b9f9d35847bba74859cc82e
316136035 <316136035@qq.com>
2024-03-06 22:51:52 +0800

    A001

--- A06_Vue-cli_脚手架/vue2_project/src/App.vue
+++ A06_Vue-cli_脚手架/vue2_project/src/App.vue
@@ -1,75 +1,31 @@
 <!-- 模版 -->
 <template>
-  <!-- #app -->
   <div id="app">
-    <!-- 组件 -->
-    <DefaultSlot>
-      <!-- template标签不显示在  slot="DefaultSlot"使用插槽  slot-scope= "AcceptFoods">接受子组件传过来的参数(返回的是对象) -->
-      <template slot="DefaultSlot" slot-scope="AcceptFoods">
-        <ul>
-          <!-- 遍历数组 -->
-          <li v-for="(item, index) in AcceptFoods.foods.carBrands" :key="index">
-            {{ item }}
-          </li>
-        </ul>
-      </template>
-    </DefaultSlot>
-
-    <DefaultSlot>
-      <template slot="DefaultSlot" slot-scope="AcceptFoods">
-        <ol>
-          <li v-for="(item, index) in AcceptFoods.foods.movies" :key="index">
-            {{ item }}
-          </li>
-        </ol>
-      </template>
-    </DefaultSlot>
-
-    <DefaultSlot>
-      <template slot="DefaultSlot" slot-scope="AcceptFoods">
-        <ul>
-          <li
-            v-for="(item, index) in AcceptFoods.foods.phoneBrands"
-            :key="index"
-          >
-            {{ item }}
-          </li>
-        </ul>
-      </template>
-    </DefaultSlot>
+      <JdPageDisplay></JdPageDisplay>
   </div>
 </template>
 
-
 <script>
 // 引入组件
-import DefaultSlot from "./components/DefaultSlot";
+import JdPageDisplay from './components/JdPageDisplay.vue'
 // 暴露组件
 export default {
-  name: "App", //自定义组件名称
+  name: 'App',//自定义组件名称
   // 注册组件
   components: {
-    DefaultSlot: DefaultSlot,
-  },
-  data() {},
-};
+    JdPageDisplay: JdPageDisplay 
+  }
+}
 </script>
 
 <!-- css样式 -->
 <style>
 #app {
-  /* 布局 */
-  display: flex;
-  /* 间距 */
-  justify-content: space-around;
-}
-img {
-  /* 宽度 */
-  width: 200px;
-  /* 高度 */
-  height: 200px;
-
-  /* 设置背景不重复 */
-  background-repeat: no-repeat;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- A06_Vue-cli_脚手架/vue2_project/src/main.js
+++ A06_Vue-cli_脚手架/vue2_project/src/main.js
@@ -6,9 +6,10 @@ import Vue from 'vue'
 import App from './App.vue'
 // 开发环境关掉提示
 Vue.config.productionTip = false
+import store from './store/index.js'
 //创建一个Vue实例
 new Vue({
-
+  store, //使用store
   /*模板解释器 引入完整版的vue 使用app组件
     1.在脚手架中node_modules\vue\package.json文件中的main.js文件中，可以看到，（ "module": "dist/vue.runtime.esm.js",）
     2. main.js文件中引入了vue.runtime.xxx.js文件不是完整的Vue.js，所以不能使用template配置项，需要使用render函数接收到的createElement函数去指定具体内容。
