0e874fd499c97a4c07ee50ae7997d700a384794c
316136035 <316136035@qq.com>
2024-03-06 22:55:59 +0800

    A001

---
+++ A06_Vue-cli_脚手架/vue2_project/A016_src_Vuex插件(数据共享_集中管理_模块管理_任意组件间通讯)/A001_Vuex插件(单组件实现)/App.vue
@@ -0,0 +1,31 @@
+<!-- 模版 -->
+<template>
+  <div id="app">
+      <JdPageDisplay></JdPageDisplay>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import JdPageDisplay from './components/JdPageDisplay.vue'
+// 暴露组件
+export default {
+  name: 'App',//自定义组件名称
+  // 注册组件
+  components: {
+    JdPageDisplay: JdPageDisplay 
+  }
+}
+</script>
+
+<!-- css样式 -->
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ A06_Vue-cli_脚手架/vue2_project/A016_src_Vuex插件(数据共享_集中管理_模块管理_任意组件间通讯)/A001_Vuex插件(单组件实现)/main.js
@@ -0,0 +1,21 @@
+
+//main.js文件是整个项目的入口文件
+//引入Vue
+import Vue from 'vue'
+//引入App.vue文件（主组件）
+import App from './App.vue'
+// 开发环境关掉提示
+Vue.config.productionTip = false
+import store from './store/index.js'
+//创建一个Vue实例
+new Vue({
+  store, //使用store
+  /*模板解释器 引入完整版的vue 使用app组件
+    1.在脚手架中node_modules\vue\package.json文件中的main.js文件中，可以看到，（ "module": "dist/vue.runtime.esm.js",）
+    2. main.js文件中引入了vue.runtime.xxx.js文件不是完整的Vue.js，所以不能使用template配置项，需要使用render函数接收到的createElement函数去指定具体内容。
+    render函数完整写法（createElement创建元素）
+    render: function(createElement) {
+    return createElement(App)}*/
+  // render函数简写（箭头函数）
+  render: h => h(App),
+}).$mount('#app')//挂载到id为app的元素上
