4b00436a127fe11a3edad318ada3f3d1748dede6
316136035 <316136035@qq.com>
2024-03-13 16:53:38 +0800

    A001

---
+++ A06_Vue-cli_脚手架/vue2_project/A20_src_Router(缓存组件)/page/FrontPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="clear-both">
+    <div class="title float-left">
+      <li>
+        <ul>
+            <!-- router-link路由路径 to="/Front" 切换到哪个页面  可以写链接也可以写对象{path:'/Front' name:'Front'}-->
+          <!-- replace属性它不会将新的路由记录添加到浏览器的历史记录栈中，相反，replace属性会触发router.replace()方法而不是router.push()方法来执行页面跳转。 -->
+          <router-link to="/frontpage/frontpage_brand">品牌</router-link>
+        </ul>
+        <ul>
+           <!-- router-link路由路径 to="/Front" 切换到哪个页面  可以写链接也可以写对象{path:'/Front' name:'Front'}-->
+          <!-- replace属性它不会将新的路由记录添加到浏览器的历史记录栈中，相反，replace属性会触发router.replace()方法而不是router.push()方法来执行页面跳转。 -->
+          <router-link to="/frontpage/frontPage_introduction">简介</router-link>
+        </ul>
+      </li>
+    </div>
+    <!-- 路由视图 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FrontPage",
+  components: {},
+  mounted() {
+    console.log("一级路由(首页)组件加载完成");
+  },
+  beforeDestroy() {
+    console.log("一级路由(首页)组件销毁完成");
+  },
+};
+</script>
+
+<style scoped>
+.title {
+  width: 100px;
+  height: 800px;
+
+  background-color: brown;
+}
+
+.Front_page {
+  background-color: rgb(4, 187, 92);
+  /* 居中 */
+  margin: 0 auto;
+
+}
+</style>
---
+++ A06_Vue-cli_脚手架/vue2_project/A20_src_Router(缓存组件)/page/Monitor.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>监控</div>
+  <input type="text" >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Monitor",
+  mounted() {
+    console.log("监控组件加载完成");
+  },
+  beforeDestroy() {
+    console.log("监控组件销毁完成");
+  },
+};
+</script>
+
+<style>
+</style>
