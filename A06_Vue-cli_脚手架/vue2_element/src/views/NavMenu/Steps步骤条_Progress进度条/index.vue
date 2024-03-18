8ffd9184bb2197daa4b2a534db83e5a2a55b5404
316136035 <316136035@qq.com>
2024-03-18 13:31:32 +0800

    A001

--- A06_Vue-cli_脚手架/vue2_element/src/views/NavMenu/Steps步骤条_Progress进度条/index.vue
+++ A06_Vue-cli_脚手架/vue2_element/src/views/NavMenu/Steps步骤条_Progress进度条/index.vue
@@ -1,20 +1,74 @@
 <template>
   <div>
     <div>
-      <el-steps :active="active" finish-status="success">
-        <el-step title="步骤 1"></el-step>
-        <el-step title="步骤 2"></el-step>
-        <el-step title="步骤 3"></el-step>
+      <div>Steps 步骤条</div>
+      <el-steps :active="active"> </el-steps>
+      <el-steps :space="200" :active="active">
+        <el-step
+          :title="item.title"
+          :description="item.description"
+          v-for="(item, i) in spaceLists"
+          :key="i"
+        ></el-step>
       </el-steps>
-
-      <el-button style="margin-top: 12px" @click="next">下一步</el-button>
+      <el-button type="text"> </el-button>
+      <el-button style="margin-top: 12px" @click="space">下一步</el-button>
     </div>
-    <div></div>
+    <div>
+      <div>Progress 进度条</div>
+      <el-progress :percentage="percentage" ></el-progress>
+
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { TimePicker } from 'element-ui';
+
+export default {
+  name: "StepsProgress",
+  data() {
+    return {
+      active: 0,
+      spaceLists: [
+        { title: "步骤1", description: "描述性文字1", status: "finish" },
+        { title: "步骤2", description: "描述性文字2", status: "finish" },
+        { title: "步骤3", description: "描述性文字3", status: "finish" },
+        { title: "步骤4", description: "描述性文字4", status: "finish" },
+        { title: "步骤5", description: "描述性文字5", status: "finish" },
+      ],
+      percentage:0
+    };
+
+  },
+  methods: {
+    space() {
+      //判断是否到最后一项
+      if (this.active++ == this.spaceLists.length - 1) {
+        this.active = 0;
+        this.Message()
+      }
+    },
+    Message(){
+      this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${ action }`
+            });
+          }
+        });
+  },
+  },
+  mounted() {
+    setInterval(() => {
+         if(this. percentage>=100){this. percentage=0};
+        this. percentage++
+        console.log(this. percentage);
+      }, 200)
+  },
+};
 </script>
 
 <style>
