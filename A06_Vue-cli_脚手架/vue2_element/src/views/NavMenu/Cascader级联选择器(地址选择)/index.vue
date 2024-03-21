08be906bef876621eb719ec26ae2615662c0fde5
316136035 <316136035@qq.com>
2024-03-21 18:41:27 +0800

    A001

--- A06_Vue-cli_脚手架/vue2_element/src/views/NavMenu/Cascader级联选择器(地址选择)/index.vue
+++ A06_Vue-cli_脚手架/vue2_element/src/views/NavMenu/Cascader级联选择器(地址选择)/index.vue
@@ -1,56 +1,85 @@
 <template>
-  <el-cascader :options="areaData" :props="{ lazy: true, lazyLoad: loadCascaderOptions }"></el-cascader>
+  <div>
+ <!-- 使用Element UI的Cascader组件 -->
+ <el-cascader
+    v-model="selectedArea"
+    :options="provinces"
+    :props="{ lazy: true, lazyLoad: lazyLoad }"
+    placeholder="请选择省市区"
+  ></el-cascader>
+  <el-button type="primary" @click="getAllAddress">获取选中省市区</el-button>
+  </div>
+ 
 </template>
 
 <script>
-import axios from '@/api/axios.js';
+import axios from 'axios'; // 引入axios用于HTTP请求
+
 export default {
   data() {
     return {
-      areaData: [], // 初始化顶级地区数据
-      selectedAddress: [], // 选中的地址ID数组
+      selectedArea: [], // 当前选中的省市区数组
+      provinces: [], // 存储顶级（省级）数据
     };
   },
-  
   methods: {
     /**
-
-     * 加载下级地区的异步方法
-     * @param {Object} treeNode - 当前节点信息
-     * @param {Function} callback - 加载完成后的回调函数
+     * 加载子级区域数据的方法，由Cascader组件调用
+     * @param {object} node 父级节点信息
+     * @param {function} resolve 回调函数，用来传递子级区域数据
      */
-    async loadCascaderOptions (treeNode, callback) {
-   console.log(treeNode);
-   console.log(callback);
-      const response = await axios.get(`getAddress/area/get?fid=${treeNode.fid}`);
-      console.log(response.data);
-      // 处理API响应数据
-     
+    async lazyLoad(node, resolve) {
+       if (!node.value) { return}
+       console.log(JSON.stringify(this.selectedArea))
+      const parentId = node.value; // 获取当前父级ID
+      const response = await axios.get(`/getAddress/area/get?fid=${parentId}`);
+      if (!response.data) { return}
+      const children = response.data.map(item => ({
+        value: item.id,
+        label: item.name,
+        pid: parentId
+      }));
 
-      // 通知Cascader组件数据加载完成
-      // callback(subAreas);
+      // 在原始数据中找到当前父级节点，并将其子级数据添加进去
+      const parentNode = this.provinces.find(province => province.value === parentId);
+      if (parentNode && !parentNode.children) {
+        parentNode.children = [];
+      }
+      if (parentNode && parentNode.children) {
+        parentNode.children.push(...children);
+      }
+
+      resolve(children); // 将子级数据传递给Cascader组件
     },
 
     /**
-     * 初始化顶级地区数据
+     * 在组件初始化时加载顶级区域数据
      */
-    async initTopLevelAreas() {
-      try {
-        const topLevelResponse = await axios.get(`getAddress/area/get?fid=4744`);
-        this.areaData = topLevelResponse.data.map(area => ({
-          value: area.id,
-          label: area.name,
-          sub_areas: []
-        }));
-        console.log(this.areaData );
-      } catch (error) {
-        console.error('初始化顶级地区数据时出错:', error);
-      }
+    async fetchProvinces() {
+      const response = await axios.get(`/getAddress/area/get?fid=4744`);
+      this.provinces = response.data.map(item => ({
+        value: item.id,
+        label: item.name,
+        children: [] // 初始化顶级区域的children为空数组
+      }));
+
+      // 由于懒加载机制，需在数据加载完成后手动触发一次更新，使顶级数据生效
+      this.$nextTick(() => {
+        this.$refs.cascader.resetFields();
+      }); 
     },
   },
-  
-  async created() {
-    await this.initTopLevelAreas(); // 组件创建时初始化顶级地区数据
+  created() {
+    this.fetchProvinces(); // 页面加载时触发获取省级数据
   },
+  getAllAddress(){
+    console.log(this.selectedArea)
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$refs.cascader.resetFields();
+    });
+  },
+  ref: 'cascader'
 };
 </script>
