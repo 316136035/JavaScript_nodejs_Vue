46e645a960a038cd3de04403f7613ee9de04384d
316136035 <316136035@qq.com>
2024-03-21 10:36:44 +0800

    A001

--- A06_Vue-cli_脚手架/vue2_element/src/views/NavMenu/Cascader级联选择器(地址选择)/index.vue
+++ A06_Vue-cli_脚手架/vue2_element/src/views/NavMenu/Cascader级联选择器(地址选择)/index.vue
@@ -1,50 +1,73 @@
 <template>
-  <el-cascader  :options="options"  
-  :props="{ value: 'id', label: 'name', children: 'children' }" 
-  lazy 
-  :lazy-load="loadAreas">
-  </el-cascader>
+  <el-cascader
+    ref="cascader"
+    :options="areaOptions"
+    :props="{ value: 'id', label: 'name', children: 'children',lazyLoad:'loadAreas'}"
+    lazy
+    lazyLoad="loadAreas"
+    placeholder="请选择地区"
+    v-model="selectedAreas"
+    @change="onAreaChange"
+  ></el-cascader>
 </template>
 
-
-
 <script>
-import axios from "axios";
-let id = 0;
+import axios from 'axios';
 
 export default {
   data() {
     return {
-      options: [],
+      areaOptions: [], // 初始地区数据
+      selectedAreas: [], // 当前选中的地区ID
     };
   },
+  
   methods: {
-    loadAreas(){},
+    
+    /**
+     * 加载下级地区的异步方法，与Cascader组件的lazy-load属性配合使用
+     * @param {Object} treeNode - 当前节点信息
+     * @param {Function} callback - 加载完成后的回调函数
+     */
+    async lazyLoad(treeNode, callback) {
+      console.log('加载下级地区：', treeNode);
+      const fid = treeNode.node.fid; // 获取父节点的ID
+      const response = await axios.get(`/getAddress/area/get?fid=${fid}`);
 
+      // 将响应数据转换为Cascader组件所需的格式
+      const subAreas = response.data.children.map(area => ({
+        value: area.id,
+        name: area.name,
+        children: [] // 注意：这里依然需要一个空数组，因为Cascader组件需要知道这是一个有子项的节点
+      }));
 
+      // 调用回调函数，传入子地区数据
+      callback(subAreas);
+    },
 
     /**
-     * 初始化顶级地区数据
+     * 选择地区发生改变时的处理方法
+     * @param {Array} values - 用户选择的地区ID数组
      */
-    async initTopLevelAreas() {
-      try {
-        const topLevelResponse = await axios.get(
-          "/getAddress/area/get?fid=4744"
-        );
-        this.options= topLevelResponse.data.map((item) => ({
-          pid: 4744,
-          value: item.id,
-          name: item.name,
-          children: [],
-        }) );
-       
-      } catch (error) {
-        console.error("初始化顶级地区数据时出错:", error);
-      }
+    onAreaChange(values) {
+      console.log('地区选择已变更：', values);
+      // 在这里可以执行其他操作，比如保存选择、进一步请求详细数据等
+    },
+    
+    // 初始化时获取顶级地区数据
+    async fetchTopLevelAreas() {
+      const topLevelResponse = await axios.get('/getAddress/area/get?fid=4744');
+    console.log(topLevelResponse);
+      this.areaOptions = topLevelResponse.data.map(area => ({
+        value: area.id,
+        name: area.name,
+        children :[]
+      }));
     },
   },
+  
   async created() {
-    await this.initTopLevelAreas(); // 组件创建时初始化顶级地区数据
+    await this.fetchTopLevelAreas();
   },
 };
 </script>
